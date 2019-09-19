ff8eb33beccfd39b3d359a6cfceee83bd4a7be69
dawidbroda <azylsound@gmail.com>
2019-09-19 22:25:30 +0200

    rl with socket

--- src/client/client.js
+++ src/client/client.js
@@ -1,16 +1,28 @@
 'use strict';
 const WebSocket = require('ws');
 const JR = require('@etk/jsonrpc');
+const readline = require('readline');
 
 const jr = new JR;
 const request = jr.Request();
+const socket = new WebSocket('socket://localhost:8080');
 const userName = 'User' + Math.floor(Math.random() * 6) + 1;
 
-console.log('You are connected as ' + userName);
-request.setResource(userName).setMethod('all').setParams({'message' : 'value'});
 
-const socket = new WebSocket('socket://localhost:8080');
+
+
+console.log('You are connected as ' + userName);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 socket.on('open', function open(){
-    socket.send(request.toString());
+    rl.on('line', (writeMessage) => {
+        console.log(`You: ${writeMessage}`);
+        request.setResource(userName).setMethod('all').setParams({'message' : writeMessage});
+        socket.send(request.toString());
+    });
 })
+
